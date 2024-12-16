e4bb6d9633468917665f1a345c8bb5b0b9b42066
Codemotozu <103793177+codemotozu@users.noreply.github.com>
2024-12-15 19:30:45 -0500

    Update __init__.py

--- camera_movement_estimator/__init__.py
+++ camera_movement_estimator/__init__.py
@@ -1,1 +1,3 @@
+# Import necessary modules and classes for the program
+# This imports a custom module `CameraMovementEstimator` from the current package
 from .camera_movement_estimator import CameraMovementEstimator
