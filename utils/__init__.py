68f480235656d1a2f1fbb67af60c12ecbe31745e
Codemotozu <103793177+codemotozu@users.noreply.github.com>
2024-12-15 22:11:37 -0500

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,14 @@
+# Importing utility functions from the 'video_utils' module.
+# These functions are used to handle video-related tasks such as reading and saving videos.
 from .video_utils import read_video, save_video
-from .bbox_utils import get_center_of_bbox, get_bbox_width, measure_distance,measure_xy_distance,get_foot_position
+
+# Importing utility functions from the 'bbox_utils' module.
+# These functions are related to bounding box operations, such as calculating the center of the box,
+# measuring distances between points, and estimating foot position.
+from .bbox_utils import (
+    get_center_of_bbox,  # Function to get the center coordinates of a bounding box.
+    get_bbox_width,  # Function to get the width of a bounding box.
+    measure_distance,  # Function to measure the Euclidean distance between two points.
+    measure_xy_distance,  # Function to measure the distance between two points on the x and y axes separately.
+    get_foot_position  # Function to estimate the foot position of a player, presumably based on their bounding box.
+)
