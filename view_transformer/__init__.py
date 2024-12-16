1c5fa558e62e26847144419b212bbd3e0eab969f
Codemotozu <103793177+codemotozu@users.noreply.github.com>
2024-12-16 04:57:44 -0500

    Update __init__.py

--- view_transformer/__init__.py
+++ view_transformer/__init__.py
@@ -1,1 +1,4 @@
+# Importing the `ViewTransformer` class from the `view_transformer` module located in the same package (indicated by the dot `.`, which means the current directory or package).
+# The `ViewTransformer` class is likely responsible for transforming the perspective or view of objects or scenes in a video.
+# It could be used for tasks such as mapping objects in a frame from one perspective to another (e.g., top-down view from a side view).
 from .view_transformer import ViewTransformer
