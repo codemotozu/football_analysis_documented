8882dba3febc777b259f85ad8f817c239c92d4d3
Codemotozu <103793177+codemotozu@users.noreply.github.com>
2024-12-15 21:39:29 -0500

    Update __init__.py

--- team_assigner/__init__.py
+++ team_assigner/__init__.py
@@ -1,1 +1,4 @@
+# Importing the TeamAssigner class from the 'team_assigner' module.
+# The '.' before the module name indicates that 'team_assigner' is a relative import,
+# meaning it's located within the same package or directory as the current file.
 from .team_assigner import TeamAssigner
